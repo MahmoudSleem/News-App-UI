51e00ef22c1b5223a4f06cbb64e6e91548b1e6a7
MahmoudSleem <sleem.mz@gmail.com>
2020-10-13 14:13:03 +0300

    add favorites window

--- lib/screens/home_tabs/favorites.dart
+++ lib/screens/home_tabs/favorites.dart
@@ -1,14 +1,153 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
 class Favorites extends StatefulWidget {
   @override
   _FavoritesState createState() => _FavoritesState();
 }
 
 class _FavoritesState extends State<Favorites> {
+  List<Color> colorsList = [
+    Colors.red,
+    Colors.teal,
+    Colors.yellow,
+    Colors.indigo,
+    Colors.brown,
+    Colors.deepOrange
+  ];
+  Random random = Random();
+
+  Color _getRandomColor() {
+    return colorsList[random.nextInt(colorsList.length)];
+  }
+
   @override
   Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, position) {
+        return Card(
+            child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: <Widget>[
+              _authorRow(),
+              _newsItemRow(),
+            ],
+          ),
+        ));
+      },
+      itemCount: 20,
+    );
+  }
+
+  Widget _authorRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: ExactAssetImage('assets/images/pg1.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                  width: 50,
+                  height: 50,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Mahmoud Sleem',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          '15 Min',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Lif Style',
+                          style: TextStyle(
+                            color: _getRandomColor(),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+        IconButton(icon: Icon(Icons.bookmark_border), onPressed: () {})
+      ],
+    );
+  }
+
+  Widget _newsItemRow() {
     return Container(
-      color: Colors.yellow,
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 125,
+            height: 100,
+            child: Image(
+              image: ExactAssetImage('assets/images/pg1.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: Column(
+                children: [
+                  Text(
+                    "Tech tent : old phones and safe social ",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'we also talk about the future of work as the robot advance',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                      height: 1.25,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
